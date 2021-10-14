e10d8956151f3e37f07ee374957da53d90a6fcd6
Liseidy Bueno <lb3347@columbia.edu>
2021-10-14 04:11:01 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,12 +9,12 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = Movie.all_ratings
     #checkbox logic goes here
-    if params[:ratings].nil?
-      selected_ratings = @all_ratings
-    else
-      selected_ratings = params[:ratings].keys
-    end
-    @movie = Movie.with_ratings(selected_ratings)
+#     if params[:ratings].nil?
+#       selected_ratings = @all_ratings
+#     else
+#       selected_ratings = params[:ratings].keys
+#     end
+#     @movie = Movie.with_ratings(selected_ratings)
     
   end
 
